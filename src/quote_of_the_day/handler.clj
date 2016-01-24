a977ceed02c75f2f41d6f05f1d324b453a62ed0d
Eeyurrn <iansergeanttan@gmail.com>
2016-01-24 20:42:29 +1100

    initial commit, hello world can count with each request

---
+++ src/quote_of_the_day/handler.clj
@@ -0,0 +1,13 @@
+(ns quote-of-the-day.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+(def count (atom 0))
+
+(defroutes app-routes
+  (GET "/" [] (swap! count inc)
+       (str "Hello World " @count))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
